49d8bc11b317015d2ce650a66fb4dd6944e00f47
Jisu Yee <103280924+rosycoloredglasses@users.noreply.github.com>
2022-06-16 02:46:18 +0000

    I made a hangman program! :D This took me a long time, and I had ot find a creative solution.

--- main.py
+++ main.py
@@ -1,11 +1,84 @@
 """
-Name(s):
-Name of Project:
+Name(s): Jisu
+Name of Project: Hangman :)
 """
+import random
+import page1
+word = random.choice(page1.lis)
+list(word)
 
-#Write the main part of your program here. Use of the other pages is optional.
+print("Welcome to Hangman, except there is no hangman. I know, I was disappointed as well. If you haven't got the word after all the guesses, type in a random character to see the answer. Here is your word.")
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+spaces = "*"*(len(word))
+print(spaces)
+print("There are four letters in this word.")
+
+firstletter = ["h", "p", "f", "b", "t", "w", "g", "y", "s", "d"]
+guesses = 6
+
+lettersguessed = []
+
+def subtract():
+  subtraction = guesses
+  print("You started with 6 guesses. I'm sure you've already figured out the word. Here are the amount of guesses you have remaining:")
+  return subtraction
+  
+listspaces = ["*","*","*","*"]
+
+letter = str(input("Enter a new letter: "))
+
+while guesses != 0:
+  if letter in firstletter:
+    if letter in list(word):
+      list(listspaces)
+      listspaces.insert(0, letter)
+      listspaces.pop(1)
+      ' '.join(listspaces)
+      guesses = guesses - 1
+      print(' '.join(listspaces))
+      print(subtract())
+      list(listspaces)
+      letter = str(input("Enter a new letter: "))
+    else:
+      print("That input is not present in the word. Please try again.")
+      guesses = guesses - 1
+      print(subtract())
+      letter = str(input("Enter a new letter: "))
+  elif letter == "e":
+      list(listspaces)
+      listspaces.insert(1, "e")
+      listspaces.pop(2)
+      ' '.join(listspaces)
+      guesses = guesses - 1
+      print(' '.join(listspaces))
+      print(subtract())
+      list(listspaces)
+      letter = str(input("Enter a new letter: "))
+  elif letter == "a":
+      list(listspaces)
+      listspaces.insert(2, "a")
+      listspaces.pop(3)
+      ' '.join(listspaces)
+      guesses = guesses - 1
+      print(' '.join(listspaces))
+      print(subtract())
+      list(listspaces)
+      letter = str(input("Enter a new letter: "))
+  elif letter == "r":
+      list(listspaces)
+      listspaces.insert(3, "r")
+      listspaces.pop(4)
+      ' '.join(listspaces)
+      guesses = guesses - 1
+      print(' '.join(listspaces))
+      print(subtract())
+      list(listspaces)
+      letter = str(input("Enter a new letter: "))
+  else:
+      print("That letter is not present in the word. Please try again.")
+      guesses = guesses -1
+      print(subtract())
+      letter = str(input("Enter a new letter: "))
+if guesses == 0:
+  print("Here is the answer: ")
+  print(word)
--- page1.py
+++ page1.py
@@ -1,1 +1,3 @@
 #Use of this page is optional. If you use code here, make sure either import page1 or from page1 import * appear on your main.py page.
+
+lis = ["pear", "fear", "bear", "tear", "wear", "gear", "hear", "year", "sear", "dear"]
